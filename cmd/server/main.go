e970da029857d177dd8bf58fbaccaed3a5845b68
Pham Hoang Anh <anhbumbk@gmail.com>
2024-08-26 00:04:57 +0900

    logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/Anhbman/microservice-server-cake/internal/controller"
+	"github.com/Anhbman/microservice-server-cake/internal/hooks"
 	"github.com/Anhbman/microservice-server-cake/internal/server/cake"
 	"github.com/Anhbman/microservice-server-cake/internal/storage"
 	"github.com/Anhbman/microservice-server-cake/rpc/service"
@@ -15,6 +17,6 @@ func main() {
 	db := storage.GetDB()
 	cakeHandle := cake.NewProcessor(db)
 	handle := controller.NewServiceServer(cakeHandle)
-	server  := service.NewServiceServer(handle)
+	server := service.NewServiceServer(handle, hooks.LoggingHooks(os.Stderr))
 	http.ListenAndServe(":8081", server)
 }
