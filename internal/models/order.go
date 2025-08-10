084ebdc942b4acbfdf1d49278705f546ae5290a8
hoanganhpham <anhbumbk@gmail.com>
2025-08-10 16:33:44 +0900

    create order models

--- internal/models/order.go
+++ internal/models/order.go
@@ -1,1 +1,13 @@
 package models
+
+import "gorm.io/gorm"
+
+type Order struct {
+	gorm.Model
+	CakeID   int64 `json:"cake_id" gorm:"not null"`
+	Cake     Cake  `json:"cake"`
+	UserID   int64 `json:"user_id" gorm:"not null"`
+	User     User  `json:"user"`
+	Quantity int64 `json:"quantity" gorm:"not null"`
+	Price    int64 `json:"price" gorm:"not null"`
+}
